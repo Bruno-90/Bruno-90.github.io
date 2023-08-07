62615ca2b7d0b799b217a7a18fade6343e78423e
Bruno-90 <67373365+Bruno-90@users.noreply.github.com>
2023-08-06 21:20:00 -0300

    Add files via upload

---
+++ Reto03/main.js
@@ -0,0 +1,33 @@
+
+    let cantidad = 0
+  function checkear(a){
+    let tareaRealizada = document.getElementById(a)    
+    let tareaT = document.getElementById('tareaTerminada')
+    let nombre2 = document.getElementById('nombre')
+    let completo = document.getElementById('completo')
+    if(nombre2.value==''){
+        alert('Ingrese algun nombre por favor')
+}
+    else{
+    cantidad ++
+    
+   
+    tareaRealizada.style.textDecoration='line-through'
+    document.getElementById('enlistado').style.display='flex'
+    tareaT.innerHTML = nombre2.value+' Cumpliste con la tarea'+' '+tareaRealizada.innerText
+    }
+    if(cantidad==10){
+        completo.innerHTML='!!!Felicitaciones cumplistes las 10 tareas!!!'
+ }
+}
+
+
+    function foco(elemento){
+        elemento.style.border = "3px solid red"
+        elemento.style.background = "gray"
+     }
+     function cambioColor(elemento){
+        elemento.style.background = "#A5FF33"
+        elemento.style.border = "2px solid green"
+     } 
+     
